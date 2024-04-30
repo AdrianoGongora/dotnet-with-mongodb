d789de897a5509c62bf8fb0ad3ec9ead5d979447
AdrianoGongora <adriano.gongora.juarez@gmail.com>
2024-04-29 20:44:03 -0500

    Example .NET with mongodb MVC

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,68 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController(BooksService booksService) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<List<Book>> Get() =>
+        await booksService.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return book;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        await booksService.CreateAsync(newBook);
+
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
+        var book = await booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        updatedBook.Id = book.Id;
+
+        await booksService.UpdateAsync(id, updatedBook);
+
+        return CreatedAtAction(nameof(Get), id, updatedBook);
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        await booksService.RemoveAsync(id);
+
+        return NotFound();
+    }
+}
